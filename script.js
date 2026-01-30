aa183e1cca935a3a3fda28992e071a21586437e3
sean <sean.zinger1999@gmail.com>
2026-01-30 14:05:13 +0200

    Add size button event handler

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// create html handles
 const canvas = document.querySelector("#canvas");
+const sizeButton = document.querySelector("#size-button");
 
 // create a default 16x16 canvas
 let sideLength = 16;
@@ -7,5 +9,15 @@ for (let i = 0; i < sideLength; i++) {
     canvas.appendChild(div);
 }
 
+// canvas mouse hover
 
 
+// size button event handler
+sizeButton.addEventListener("click", (sideLength) => {
+    do {
+        sideLength = prompt("Enter canvas side length (1-100): ");
+        if (sideLength < 1 || sideLength > 100) {
+            alert("Please enter a side length within bounds!");
+        }
+    } while (sideLength < 1 || sideLength > 100);
+});
