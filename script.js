0ad03dad09366fc7a41a99e50e007a4524090d7e
sean <sean.zinger1999@gmail.com>
2026-01-30 17:25:25 +0200

    Finish canvas generation
    moved the canvas creation to a function.
    fixed a bug when modifying the canvas size; the old canvas
    wasn't deleted inside the button event handler. now it
    deletes the old canvas and recreates a new one with the new
    provided side length.

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+const DIV_SIZE = 10; // 10px
+
 // create html handles
 const canvas = document.querySelector("#canvas");
 const sizeButton = document.querySelector("#size-button");
 
 // create a default 16x16 canvas
-let sideLength = 16;
-for (let i = 0; i < sideLength; i++) {
-    const div = document.createElement("div");
-    canvas.appendChild(div);
-}
-
-// canvas mouse hover
-
+createCanvas(canvas);
 
 // size button event handler
-sizeButton.addEventListener("click", (sideLength) => {
+sizeButton.addEventListener("click", () => {
+    // delete old canvas first
+    canvas.innerHTML = "";
+
+    // get canvas' side length from user
+    let sideLength = 0;
     do {
-        sideLength = prompt("Enter canvas side length (1-100): ");
+        sideLength = +prompt("Enter canvas side length (1-100): ");
+        canvas.style.width = `${sideLength * DIV_SIZE}px`;
+        canvas.style.height = `${sideLength * DIV_SIZE}px`;
+
         if (sideLength < 1 || sideLength > 100) {
             alert("Please enter a side length within bounds!");
         }
     } while (sideLength < 1 || sideLength > 100);
+
+    // create new canvas
+    createCanvas(canvas, sideLength);
 });
+
+// a function that gets a parent handler (canvas) and a length of its side.
+// then fills the parent with (sideLength ** 2) div elements.
+// creates a default 16x16 canvas if side length is not provided.
+function createCanvas(parentElement, sideLength = 16) {
+    for (let i = 0; i < sideLength; i++) {
+        for (let j = 0; j < sideLength; j++) {
+            const div = document.createElement("div");
+            parentElement.appendChild(div);
+        }
+}
+parentElement.style.width = `${sideLength * DIV_SIZE}px`;
+parentElement.style.height = `${sideLength * DIV_SIZE}px`;
+}
