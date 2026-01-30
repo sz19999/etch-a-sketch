47ab4d770c95f442aa22be2197af0ef037ca3839
sean <sean.zinger1999@gmail.com>
2026-01-30 13:54:41 +0200

    Initial commit
    Added index.html, script.js and styles.css files.

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector("#canvas");
+
+// create a default 16x16 canvas
+let sideLength = 16;
+for (let i = 0; i < sideLength; i++) {
+    const div = document.createElement("div");
+    canvas.appendChild(div);
+}
+
+
+
